b9bee80a042ae2585069b7e9902884fb2560320e
Радик <a-radik-87@mail.ru>
2023-01-23 14:31:14 +0300

    Добавляет изменения в хедере

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,7 @@
 const pageHeader = document.querySelector('.page-header');
-const siteNavToggle = document.querySelector('.site-nav__toggle');
+const userNavToggle = document.querySelector('.user-nav__toggle');
 
 pageHeader.classList.remove('page-header--nav-nojs');
-siteNavToggle.addEventListener('click', function() {
+userNavToggle.addEventListener('click', function() {
   pageHeader.classList.toggle('page-header--nav-open');
 });
